90aa4ecffa4e61b970654bb034c4ef446dccf8b7
wesbarron <54125356+wesbarron@users.noreply.github.com>
2022-10-04 23:34:24 -0400

    Update scheduler.py

--- scheduler.py
+++ scheduler.py
@@ -1,12 +1,50 @@
 import networkx as nx
+import re
 import courseParser
+import worksheet
+import GUI
+from openpyxl import Workbook, load_workbook
+from openpyxl.styles import Color, PatternFill, Font, Border
+from openpyxl.styles import colors
+from openpyxl.cell import Cell
 
+def getTotalCreditHours(creditHours, CoursesDict):
+    coursesInSchedule = []
+    coursesChecklist = []
+    Survey = CoursesDict["CPSC 4000"]
+    if Survey:
+        print("SEMESTER COUNT RETRIEVED")
+    for course in CoursesDict: 
+        if course != Survey.CourseNum:
+            coursesChecklist.append(CoursesDict[course])
 
+    schedule = []
+ 
+    #while(len(coursesChecklist)>0):
+    semesterCreditHours = 0
+    newSemester = []
+    poppedCourseIndexes = []
+    for i in range(0, len(coursesChecklist)):
+        #if credit hours limit is hit
+        course = coursesChecklist[i]
+        
+        if(course.Completed == False):
+            if((semesterCreditHours + course.CreditHours)<=creditHours):
+                
+                if course.canBeTaken():
+                    #print("Course is being appended: " + course.CourseNum)
+                    poppedCourseIndexes.append(i)
+                    newSemester.append(course)
+                    semesterCreditHours += course.CreditHours
+                    coursesInSchedule.append(course)
+    #print (semesterCreditHours)
+    return semesterCreditHours
 
 def setSchedule(creditHours, CoursesDict):
     print("Setting Schedule: ")
     coursesInSchedule = []
     coursesChecklist = []
+    coursesInWorkBook = []
 
     for course in CoursesDict: 
        
@@ -14,11 +52,33 @@ def setSchedule(creditHours, CoursesDict):
 
     schedule = []
 
+    #print schedule
+
+    wb = load_workbook('schedule.xlsx', keep_vba=True)
+    ws = wb.active
+    #ws.title = "Path To Graduation"
+    ws.column_dimensions['A'].width = 25
+    ws.column_dimensions['B'].width = 20
+    ws.column_dimensions['C'].width = 25
+    ws.column_dimensions['D'].width = 20
+    ws.column_dimensions['E'].width = 25
+    ws.column_dimensions['F'].width = 20
+    ws.sheet_view.showGridLines = True
+    # redFill = PatternFill(start_color='FFFF0000',
+    #                end_color='FFFF0000',
+    #                fill_type='solid')
+    hoursCount = GUI.globalHoursCount
+    print(getTotalCreditHours(hoursCount, CoursesDict))
+    creditHoursAllowed = getTotalCreditHours(hoursCount, CoursesDict)
+
     
     while(len(coursesChecklist)>0):
         semesterCreditHours = 0
         newSemester = []
         poppedCourseIndexes = []
+        workBookHours = 0
+        logHours = 0
+    
         for i in range(0, len(coursesChecklist)):
             #if credit hours limit is hit
             course = coursesChecklist[i]
@@ -40,6 +100,20 @@ def setSchedule(creditHours, CoursesDict):
                         newSemester.append(course)
                         semesterCreditHours += course.CreditHours
                         coursesInSchedule.append(course)
+                        coursesInWorkBook.clear()
+
+                        if ((logHours + course.CreditHours) <= creditHoursAllowed):
+                            j = i + 1
+                            ws.cell(j+1,1).value = course.CourseNum
+                            ws.cell(j+1,2).value = course.CreditHours
+                            #print("This is the course number : " + course.CourseNum)
+                            workBookHours += course.CreditHours
+                            coursesInWorkBook.append(course.CourseNum)
+                            
+                        logHours = workBookHours + course.CreditHours
+                        #print("log Hours : " + str(logHours))
+                        
+                
             else:
                 poppedCourseIndexes.append(i)
             poppedCourseIndexes.sort(reverse=True)
@@ -47,7 +121,7 @@ def setSchedule(creditHours, CoursesDict):
             for index in poppedCourseIndexes:
                 coursesChecklist.pop(index)
             schedule.append(newSemester)
-        
+        wb.save("scheduleResult.xlsx") 
 
 
     # if(len(coursesChecklist)>0):
@@ -71,7 +145,7 @@ def setSchedule(creditHours, CoursesDict):
     #             coursesInSchedule.append(courseFromChecklist)
     #             semesterCreditHours += course.CreditHours 
 
-
+    wb.save("schedule.xlsx") 
         
     
         
